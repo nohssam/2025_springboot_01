a76f7466b8a550cf21a1b4e99b09d1fb54d52d92
nohssam <nohssam73@gmail.com>
2025-05-28 12:21:20 +0900

    0528-02 jwt 처리 중

---
+++ src/main/java/com/ict/edu01/jwt/JwtService.java
@@ -0,0 +1,10 @@
+package com.ict.edu01.jwt;
+
+import java.util.Map;
+
+import com.ict.edu01.members.vo.MembersVO;
+
+public interface JwtService {
+    Map<String,String> login(MembersVO mvo);  // 로그인처리
+    String getuserIdFromToken(String token);  // 토큰에서 사용자ID 추출
+}
